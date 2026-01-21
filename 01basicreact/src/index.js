10cd6f906c6046b664518563b05157602b528d32
arshkhatibi123-png <arshkhatibi123@gmail.com>
2026-01-21 12:36:58 +0530

    bscindxjs

--- 01basicreact/src/index.js
+++ 01basicreact/src/index.js
@@ -16,3 +16,26 @@ root.render(
 // It does not render any visible UI 
 // and has no impact on the production build 
 // or performance for your users. 
+
+// ######### root.render########
+//In React 18 and later, root.render() is the
+//  method used to display 
+// a React component inside a browser DOM element. 
+// It is the entry point for your application's UI. 
+
+//Key Points
+
+// Entry Point: root.render() is 
+// typically called only 
+// once in an application's lifecycle 
+// to perform the initial render.
+
+// DOM Management: Once called,
+//  React takes over the management 
+// of the DOM inside the specified container element.
+
+// Updates: Subsequent updates to the UI
+//  are handled internally by React 
+// through state and prop changes 
+// within your components,
+//  not by calling root.render() again.
