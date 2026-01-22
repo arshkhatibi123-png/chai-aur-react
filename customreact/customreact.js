96ec044cc72fdc5a93950bfea191ace26b4b7583
arshkhatibi123-png <arshkhatibi123@gmail.com>
2026-01-22 10:29:49 +0530

    custom

---
+++ customreact/customreact.js
@@ -0,0 +1,14 @@
+function customRender(reactElement, container){}
+const reactElement ={
+   type: 'a',
+   props: {
+    href:'http://google.com'
+    target:'_blank
+    ,
+    children:'Click me to vissit google '
+   } 
+}
+
+const mainContainer=document.querySelector('#root')// root grab,and store at variable cld main cntnr
+
+customRender(reactElement, mainContainer)
