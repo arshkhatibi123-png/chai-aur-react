066a0cabf73c149899c09c7f29d56cefeb20138c
arshkhatibi123-png <arshkhatibi123@gmail.com>
2026-01-22 15:03:10 +0530

    vitjsx

--- 01basicreact/01vitereact/src/App.jsx
+++ 01basicreact/01vitereact/src/App.jsx
@@ -1,14 +1,14 @@
 import Chai from "./Chai."
 
 
-function App() {
+function App() {   //app is a fn
  
 
   return (
     <>
    <Chai/>
 
-{/* //</><h2>chai is return<h2/> } // cnt use bcz rtrn/export only one element
+{/* //<h2>chai is return<h2/> } // cnt use bcz rtrn/export only one element
                                  // so use wd <div></div> or<> </>(called  fragment) */}
    <h2>chai is return</h2>
    <p> test para</p>
